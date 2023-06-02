56e0abdafc44f966d5ba3a2e340ceb585d1bd024
Hiram Méndez <hirammendez000@gmail.com>
2023-06-01 19:35:30 -0600

    puto brodely de mierda

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    colors: {
+      'tangBlue': '#0459c6',
+      'brandeisBlue': '#056bf0',
+      'azure': '#2380fb',
+      'dodgerBlue': '#4b97fb',
+      'ruddyBlue': '#73aefc',
+      'powderBlue': '#b2cff6',
+      'antiflashWhite': '#f0f0f0',
+      'night': '#101010'
+    }
   },
   plugins: [],
 }
